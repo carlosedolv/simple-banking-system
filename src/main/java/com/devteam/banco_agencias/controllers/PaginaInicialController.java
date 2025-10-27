1d29f6ba11db764772f1d39f57b3c50b46b91d47
Richard Antunis <richardantunis4gmail.com@Richards-MacBook-Pro.local>
2025-10-27 17:42:44 -0300

    feat: consumindo a rota delete no botao Deletar

--- src/main/java/com/devteam/banco_agencias/controllers/PaginaInicialController.java
+++ src/main/java/com/devteam/banco_agencias/controllers/PaginaInicialController.java
@@ -4,7 +4,9 @@ package com.devteam.banco_agencias.controllers;
 import com.devteam.banco_agencias.entities.Banco;
 import com.devteam.banco_agencias.services.BancoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,5 +33,12 @@ public class PaginaInicialController {
     public Banco exibirBanco(@PathVariable Long id) {
         return service.findById(id);
 
+    }
+
+    @DeleteMapping("/banco/{id}")
+    @ResponseBody
+    public ResponseEntity<Void> deletarBanco(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.ok().build();
     }
 }
