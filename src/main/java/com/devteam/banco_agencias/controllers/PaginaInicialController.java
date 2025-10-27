06bcbc096cda94dfec7e5d11ddeba4d96d1a4d3b
Richard Antunis <richardantunis4gmail.com@Richards-MacBook-Pro.local>
2025-10-27 13:56:13 -0300

    feat: adicionando pagina de formulario de cadastro do banco

--- src/main/java/com/devteam/banco_agencias/controllers/PaginaInicialController.java
+++ src/main/java/com/devteam/banco_agencias/controllers/PaginaInicialController.java
@@ -7,12 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
-public class  paginaInicialController {
+public class PaginaInicialController {
 
     @Autowired
     private BancoService service;
@@ -23,5 +24,12 @@ public class  paginaInicialController {
         List<Banco> bancos = service.findAll();
         model.addAttribute("bancos", bancos);
         return "paginaInicial";
+    }
+
+    @GetMapping("/api/bancos/{id}")
+    @ResponseBody
+    public Banco exibirBanco(@PathVariable Long id) {
+        return service.findById(id);
+
     }
 }
